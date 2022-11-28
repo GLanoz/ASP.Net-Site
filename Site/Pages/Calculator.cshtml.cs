ef480659af83032f1b36f7cfbac332ad02046f03
Vlad Lanoz <vladpisarenko97@gmail.com>
2022-11-29 01:39:41 +0200

    Add project files.

---
+++ Site/Pages/Calculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Site.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
